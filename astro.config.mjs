5182e1d603b504e2746587e83b22c7d62d51d3ba
kap <william.kapke@gmail.com>
2026-05-12 15:24:33 -0700

    Use github.io as preview URL until custom domain is ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import sitemap from '@astrojs/sitemap';
 
-const isProd = process.env.CNAME === 'true';
-
 export default defineConfig({
-  site: 'https://cuss2.com',
-  base: isProd ? '/' : '/cuss2.com',
+  site: 'https://elevationai.github.io/cuss2.com',
+  base: '/cuss2.com',
   integrations: [vue(), sitemap()],
   output: 'static',
 });
